c7a29442803953bd736528a87a32568d765ea270
Brian Ashley <brian@bigfuzz.com>
2018-11-16 12:01:57 -0500

    updates to less
    - fixed compile issue

--- src/stories/index.js
+++ src/stories/index.js
@@ -10,10 +10,42 @@ import { Button, Welcome } from '@storybook/react/demo';
   
 storiesOf('Form Fields', module)
   .add('Text Inputs', () => (
-    <div className="form-element full">
-      <label htmlFor="inputid">Field Label</label>
-      <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+    <React.Fragment>
+    <div className="form-group">
+      <div className="form-element full">
+        <label htmlFor="inputid">Field Label - Full Width</label>
+        <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+      </div>
     </div>
+    <div className="form-group">
+      <div className="form-element half">
+        <label htmlFor="inputid">Field Label - Half Width</label>
+        <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+      </div>
+      <div className="form-element half">
+        <label htmlFor="inputid">Field Label - Half Width</label>
+        <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+      </div>
+    </div>
+    <div className="form-group">
+      <div className="form-element quarter">
+        <label htmlFor="inputid">Field Label - Quarter Width</label>
+        <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+      </div>
+      <div className="form-element quarter">
+        <label htmlFor="inputid">Field Label - Quarter Width</label>
+        <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+      </div>
+      <div className="form-element quarter">
+        <label htmlFor="inputid">Field Label - Quarter Width</label>
+        <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+      </div>
+      <div className="form-element quarter">
+        <label htmlFor="inputid">Field Label - Quarter Width</label>
+        <input type="text" placeholder="Placeholder Text" name="inputid" id="inputid"/>
+      </div>
+    </div>
+    </React.Fragment>
   ));
   
 storiesOf ('Users', module)
